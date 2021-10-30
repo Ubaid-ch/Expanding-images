e376438c1e873f445e8ff7961cf5ff8446ca8bd9
Ubaid-ch <80938435+Ubaid-ch@users.noreply.github.com>
2021-10-30 15:49:19 +0500

    Add files via upload

---
+++ expanding-image/js/script.js
@@ -0,0 +1,18 @@
+const panels = document.querySelectorAll('.panel');
+// console.log(panels)
+
+panels.forEach((panel) =>{
+panel.addEventListener('click',function(){
+removeActiveClasses();
+panel.classList.add('active');
+}
+)
+
+}
+)
+
+function removeActiveClasses(){
+ panels.forEach((panel) => {
+     panel.classList.remove('active');
+ })
+}
